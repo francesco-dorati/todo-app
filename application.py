6ab67a9107dc7ab94115b3a7cf4c023114d2e522
Francesco Dorati <fd.francescodorati@gmail.com>
2021-03-23 16:09:15 +0100

    Add DELETE and Refactor

--- application.py
+++ application.py
@@ -1,20 +1,72 @@
+import os
+
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+"""
+    TODOs:
+    - change add remove response ({'added': 'todo'})
+"""
+
+file_name = 'todos.txt'
+
 @app.route('/', methods=['GET', 'POST', 'DELETE'])
 def todo():
+    # GET
     if request.method == 'GET':
         todos = []
-        with open('todos.txt') as file:
+
+        # check if file exists
+        if not os.path.isfile('todos.txt'):
+            open(file_name, 'w').close()
+
+        # read the file
+        with open(file_name) as file:
             for line in file:
                 todos.append(line.strip())
-        print(todos)
+
         return jsonify(todos)
 
+    # POST
     elif request.method == 'POST':
-        todo = request.get_json().get('todo') + '\n'
-        print(request.data)
-        with open('todos.txt', 'a') as file:
+        todo = request.form.get('todo') + '\n'
+
+        # append the line
+        with open(file_name, 'a') as file:
             file.write(todo)
-        return todo
+
+        return jsonify({'added': todo.strip()}) 
+
+    # DELETE
+    else:
+        # read file data
+        with open(file_name, 'r') as file:
+            infile = file.readlines()
+
+        # check index validity
+        index = request.args.get('index') 
+        if (not index.isnumeric() and index != 'all') or (index.isnumeric() and int(index) > len(infile)):
+            return "Invalid index", 404
+
+        # delete
+        if index == 'all':
+            # delete all
+            removed = []
+            with open(file_name, 'w') as file:
+                for line_index, line in enumerate(infile):
+                    removed.append({'text': line.strip(), 'index': line_index})
+
+            return jsonify({'removed': removed})
+        else:
+            # delete the line
+            index = int(index) - 1
+            with open(file_name, 'w') as file:
+                for line_index, line in enumerate(infile):
+                    if line_index != index:
+                        file.write(line)
+                    else:
+                        deleted = line.strip()
+    
+        return jsonify({'removed': {'text': deleted, 'index': index}})
+
