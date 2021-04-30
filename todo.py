d3dc42466cfd79cbaa29cc8e2956acc00a7154de
Francesco Dorati <fd.francescodorati@gmail.com>
2021-04-30 15:16:24 +0200

    Update Change Mode

--- todo.py
+++ todo.py
@@ -8,8 +8,6 @@ from termcolor import colored, cprint
 
 """
     TODO
-    - todo switch mode -> changes mode
-
     - load external links 
         - if remote load also remote links
     - remote compernde solo il main
@@ -57,6 +55,7 @@ if os.path.isfile(paths['main']):
         for name in links:
             paths[name] = links[name]
 
+
 def main():
     todos_color = 'grey'
     attrs = ['bold']
@@ -64,98 +63,95 @@ def main():
     # if user wrote only 1 argument
     if len(sys.argv) == 1:
         # get
-        todolist = get(config['mode'])
-        print_list('get', todolist)
+        data = get(config['mode'])
+        print_list('get', data)
 
     # if user wrote 2 or more arguments
     elif len(sys.argv) >= 2:
         option = sys.argv[1]
-        
+
         # create
         if option == 'create':
-            todolist = create('main')
-            print_list('create', todolist)
-        
+            data = create('main')
+            print_list('create', data)
+
         # add
         elif option == 'add':
             # if local
-            todolist = add(config['mode'], sys.argv[2])
-            print_list('add', todolist)
+            data = add(config['mode'], sys.argv[2])
+            print_list('add', data)
 
         # remove
         elif option == 'remove':
-            todolist = remove(config['mode'], sys.argv[2])
-            print_list('remove', todolist)
+            data = remove(config['mode'], sys.argv[2])
+            print_list('remove', data)
 
         # modes
         elif option in paths:
             # get
             if len(sys.argv) == 2:
-                todolist = get(option)
-                print_list('get', todolist)
+                data = get(option)
+                print_list('get', data)
 
             else:
                 local_option = sys.argv[2]
 
                 # create
                 if local_option == 'create':
-                    todolist = create(option)
+                    data = create(option)
 
                 # add
                 elif local_option == 'add':
-                    todolist = add(option, sys.argv[3])
+                    data = add(option, sys.argv[3])
 
                 # remove
                 elif local_option == 'remove':
-                    todolist = remove(option, sys.argv[3])
+                    data = remove(option, sys.argv[3])
 
-                print_list(local_option, todolist)
+                print_list(local_option, data)
 
-        # settings
-        elif option == 'settings':
+        # mode
+        elif option == 'mode':
             # if 2 arguments given
             if len(sys.argv) == 2:
-                print_list('settings')
+                print_list('mode')
 
-            # if 4 arguments given
-            elif len(sys.argv) == 4:
-                if sys.argv[2] == 'mode':
-                    # check invalid mode
-                    if sys.argv[3] != '1' and sys.argv[3] != '2':
-                        print_list('settings', {
-                            'error': 1,
-                            'message': 'Inalid mode.',
-                        })
+            elif sys.argv[2] in ['remote', 'main']:
+                mode = sys.argv[2]
+                # check if mode is already set
+                if mode == config['mode']:
+                    print_list('mode', {
+                        'error': 2,
+                        'message': 'Mode already set.',
+                        'mode': mode
+                    })
+                else:
+                    # write changes to file
+                    config['mode'] = mode
+                    with open(paths['config'], 'w') as file:
+                        file.write(json.dumps(config))
 
-                    mode = 'main' if sys.argv[3] == '1' else 'remote'
+                    print_list('mode', {
+                        'mode': mode
+                    })
 
-                    # check if mode is already set
-                    if mode == config['mode']:
-                        print_list('settings', {
-                            'error': 2,
-                            'message': 'Mode already set.',
-                            'mode': mode
-                        })
-
-                    else:
-                        # write changes to file
-                        config['mode'] = mode
-                        with open(paths['config'], 'w') as file:
-                            file.write(json.dumps(config))
-
-                        print_list('settings', {
-                            'mode': mode
-                        })
+            else:
+                print_list('settings', {
+                    'error': 1,
+                    'message': 'Inalid mode.',
+                })
 
         else:
             print(f'Illegal option "{sys.argv[1]}".')
             exit(1)
-        
+
     else:
         print(f'Invalid number of arguments.')
         exit(1)
 
 # create
+
+
 def create(mode, name=None):
     path = paths[mode]
 
@@ -165,10 +161,10 @@ def create(mode, name=None):
     # check if file exists
     if os.path.isfile(path):
         return {'error': 1, 'message': 'file already exists.', 'path': path}
-            
-    # create todolist
+
+    # create data
     if mode == 'local':
-        todolist = {
+        data = {
             'name': name if name else os.path.basename(os.getcwd()),
             'todos': []
         }
@@ -176,13 +172,14 @@ def create(mode, name=None):
         # add to links
         with open(paths['main'], 'r') as file:
             main = json.loads(file.read())
-            main['links'][todolist['name']] = os.getcwd() + '/' + paths['local']
+            main['links'][data['name']] = os.getcwd() + '/' + \
+                paths['local']
 
         with open(paths['main'], 'w') as file:
             file.write(json.dumps(main))
 
     else:
-        todolist = {
+        data = {
             'name': 'main',
             'todos': [],
             'links': {}
@@ -190,15 +187,15 @@ def create(mode, name=None):
 
     # write to file
     with open(path, 'w') as file:
-        file.write(json.dumps(todolist))
+        file.write(json.dumps(data))
 
-    return todolist
-                    
+    return data
+
 
 # GET
 def get(mode):
     path = paths[mode]
-    
+
     # if remote
     if mode == 'remote':
         response = requests.get(path)
@@ -213,20 +210,22 @@ def get(mode):
     # if local
     else:
         if not os.path.isfile(path):
-            return {'error': 1, 'message': 'file not found', 'path': path} 
+            return {'error': 1, 'message': 'file not found', 'path': path}
 
         # read file
         with open(path, 'r') as file:
             return json.loads(file.read())
 
 # ADD
+
+
 def add(mode, added):
     path = paths[mode]
 
     # if remote
     if mode == 'remote':
         response = requests.post(path, data={'add': added})
-                    
+
         # check for status code
         if response.status_code == 200:
             return json.loads(response.text)
@@ -238,21 +237,21 @@ def add(mode, added):
     else:
         # check for file
         if not os.path.isfile(path):
-            return {'error': 1, 'message': 'file not found', 'path': path} 
+            return {'error': 1, 'message': 'file not found', 'path': path}
 
         # get and append new todo
-        todolist = get(mode)
-        todolist['todos'].append(added)
+        data = get(mode)
+        data['todos'].append(added)
 
         # write to file
         with open(path, 'w') as file:
-            file.write(json.dumps(todolist))
+            file.write(json.dumps(data))
 
         return {
-            'name': todolist['name'],
-            'todos': todolist['todos'],
+            'name': data['name'],
+            'todos': data['todos'],
             'added': {
-                'index': len(todolist['todos']) - 1, 
+                'index': len(data['todos']) - 1,
                 'text': added
             }
         }
@@ -276,189 +275,192 @@ def remove(mode, removed):
     else:
         # check for file
         if not os.path.isfile(path):
-            return {'error': 1, 'message': 'file not found', 'path': path} 
+            return {'error': 1, 'message': 'file not found', 'path': path}
 
         # get todos
-        todolist = get(mode)
+        data = get(mode)
         removed_list = []
 
         # check index validity
         if ((not removed.isnumeric() and removed != 'all') or
-            (removed.isnumeric() and (int(removed) > len(todolist['todos']) or int(removed) <= 0))):
-            return {'error': 2, 'message': 'invalid index.', 'path': path} 
- 
+                (removed.isnumeric() and (int(removed) > len(data['todos']) or int(removed) <= 0))):
+            return {'error': 2, 'message': 'invalid index.', 'path': path}
+
         # delete all
         if removed == 'all':
-            for index, todo in enumerate(todolist['todos']):
+            for index, todo in enumerate(data['todos']):
                 removed_list.append({'index': index, 'text': todo})
-            todolist['todos'] = []
+            data['todos'] = []
 
         # delete one
         elif removed.isnumeric():
             index = int(removed) - 1
-            removed_list.append({'index': index, 'text': todolist['todos'].pop(index)})
+            removed_list.append(
+                {'index': index, 'text': data['todos'].pop(index)})
 
         # write to file
         with open(path, 'w') as file:
-            file.write(json.dumps(todolist))
+            file.write(json.dumps(data))
 
         return {
-            'name': todolist['name'],
-            'todos': todolist['todos'],
-            'removed': removed_list 
+            'name': data['name'],
+            'todos': data['todos'],
+            'removed': removed_list
         }
-            
 
-def print_list(action, todolist={}):
-    print_bold = lambda x, y=None: cprint(x, y, attrs=['bold'], end='')
-    remote_color = 'blue'
-    main_color = 'yellow'
-    local_color = 'magenta'
+
+def print_list(action, data={}):
+    def print_bold(x, y=None): return cprint(x, y, attrs=['bold'], end='')
+    mode_color = {
+        'remote': 'blue',
+        'main': 'yellow',
+        'local': 'magenta'
+    }
 
     # if no error
-    if not 'error' in todolist:
+    if not 'error' in data:
         # get add remove
         if action in ['get', 'add', 'remove', 'create']:
             # name color
-            if todolist['name'] == 'remote':
-                name_color = remote_color 
-            elif todolist['name'] == 'main':
-                name_color = main_color 
+            if data['name'] in mode_color:
+                name_color = mode_color[data['name']]
             else:
-                name_color = local_color 
+                name_color = mode_color['local']
 
             # feedback
             if action == 'add':
-                print_bold('\nAdded '), 
-                print_bold(f'"{todolist["added"]["text"]}" ', 'green') 
+                print_bold('\nAdded '),
+                print_bold(f'"{data["added"]["text"]}" ', 'green')
                 print_bold('successfully to ')
-                print_bold(todolist['name'], name_color)
+                print_bold(data['name'], name_color)
                 print_bold('!\n')
 
             elif action == 'remove':
-                print_bold('\nRemoved '), 
-                if len(todolist['todos']) == 0 and len(todolist['removed']) != 1:
-                    print_bold('all ', 'red') 
-                elif len(todolist['removed']) == 1:
-                    print_bold(f'"{todolist["removed"][0]["text"]}" ', 'red') 
+                print_bold('\nRemoved '),
+                if len(data['todos']) == 0 and len(data['removed']) != 1:
+                    print_bold('all ', 'red')
+                elif len(data['removed']) == 1:
+                    print_bold(f'"{data["removed"][0]["text"]}" ', 'red')
                 print_bold('successfully from ')
-                print_bold(todolist['name'], name_color)
+                print_bold(data['name'], name_color)
                 print_bold('!\n')
-    
+
             elif action == 'create':
-                if todolist['name'] == 'main':
+                if data['name'] == 'main':
                     print_bold('\n')
-                    print_bold(todolist['name'], name_color)
+                    print_bold(data['name'], name_color)
                     print_bold(' TODO list successfully created!\n')
                 else:
                     print_bold('\nLocal ')
-                    print_bold(todolist['name'], name_color)
+                    print_bold(data['name'], name_color)
                     print_bold(' TODO list successfully created!\n')
-    
+
             print_bold('\nYour ')
-            print_bold(todolist['name'], name_color)
+            print_bold(data['name'], name_color)
             print_bold(' TODOs:\n')
-    
+
             # add
             if action == 'add':
-                for index, todo in enumerate(todolist['todos']):
-                    if index == todolist['added']['index']:
+                for index, todo in enumerate(data['todos']):
+                    if index == data['added']['index']:
                         print_bold(f'    [+] {todo}\n', 'grey')
                     else:
                         print_bold(f'    [{index + 1}] {todo}\n', 'grey')
-    
+
             # remove
             elif action == 'remove':
-                for index, todo in enumerate(todolist['todos']):
-                    if any(removed['index'] == index for removed in todolist['removed']):
-                        print_bold(f'   [-] {todolist["removed"][index]["text"]}\n', 'red')
-                        todolist['removed'].pop(index)
-    
+                for index, todo in enumerate(data['todos']):
+                    if any(removed['index'] == index for removed in data['removed']):
+                        print_bold(
+                            f'   [-] {data["removed"][index]["text"]}\n', 'red')
+                        data['removed'].pop(index)
+
                     print_bold(f'   [{index + 1}] {todo}\n', 'grey')
-    
+
                 # print remaining removed todos
-                for todo in todolist['removed']:
+                for todo in data['removed']:
                     print_bold(f'   [-] {todo["text"]}\n', 'red')
-    
+
             # get
             else:
                 # todos present
-                if len(todolist['todos']) > 0:
-                    for index, todo in enumerate(todolist['todos']):
+                if len(data['todos']) > 0:
+                    for index, todo in enumerate(data['todos']):
                         print_bold(f'   [{index + 1}] {todo}\n', 'grey')
-    
+
                 # no todos
                 else:
                     print_bold('    [?] This TODO list is empty\n', 'grey')
                     print_bold(f'\nAdd one by running: ', 'grey')
-                    print_bold(f'todo {"local " if not todolist["name"] == "main" else ""}add <todo>\n', 'grey')
-    
-    
+                    print_bold(
+                        f'todo {"local " if not data["name"] == "main" else ""}add <todo>\n', 'grey')
+
         # settings
-        elif action == 'settings':
-            if todolist:
+        elif action == 'mode':
+            if data:
                 print_bold('\nMode set to ')
-                print_bold(todolist['mode'], 'green')
+                print_bold(data['mode'], mode_color[data['mode']])
                 print_bold('\n')
 
-            print_bold('\nTODOs settings\n')
             print_bold('\nMode:\n')
-            print_bold('[1] Main\n', 'grey' if config['mode'] == 'remote' else None)
-            print_bold('[2] Remote\n', 'grey' if config['mode'] == 'main' else None)
-            
-            if todolist:
+            print_bold('[✓]', 'green') if config['mode'] == 'main' else print_bold('[ ]')
+            print_bold(' main\n', mode_color['main'])
+            print_bold('[✓]', 'green') if config['mode'] == 'remote' else print_bold('[ ]')
+            print_bold(' remote\n', mode_color['remote'])
+
+            if data:
                 print_bold('\nNow you can access ', 'grey')
-                print_bold(todolist['mode'], 'grey')
+                print_bold(data['mode'], 'grey')
                 print_bold(' just by running: todo\n', 'grey')
             else:
-                print_bold('\nEdit with: todo settings mode <number>\n', 'grey')
-                
+                print_bold('\nEdit with: todo mode <mode>\n', 'grey')
 
     # if error
     else:
         print_bold('\nError: ', 'red')
-        if action in ['get','add', 'remove']:
+        if action in ['get', 'add', 'remove']:
             # file not found
-            if todolist['error'] == 1:
-                if todolist['path'] == paths['local']:
+            if data['error'] == 1:
+                if data['path'] == paths['local']:
                     print_bold('No ')
-                    print_bold('local', local_color)
+                    print_bold('local', mode_color['local'])
                     print_bold(' TODO list found.\n')
-                    print_bold('\nCreate one by running: todo local create [<name>]\n', 'grey')
+                    print_bold(
+                        '\nCreate one by running: todo local create [<name>]\n', 'grey')
                 else:
                     print_bold('No ')
-                    print_bold('main', main_color)
+                    print_bold('main', mode_color['main'])
                     print_bold(' TODO list found.\n')
                     print_bold('\nCreate by running: todo create\n', 'grey')
-            if action == 'remove' and todolist['error'] == 2:
+            if action == 'remove' and data['error'] == 2:
                 print_bold('Invalid index.\n')
 
         elif action == 'create':
-            if todolist['error'] == 1:
-                if todolist['path'] == paths['local']:
+            if data['error'] == 1:
+                if data['path'] == paths['local']:
                     name = get(paths['local'])['name']
-                    print_bold('local', local_color)
+                    print_bold('local', mode_color['local'])
                     print_bold(' TODO list already exists as ')
-                    print_bold(name, local_color)
+                    print_bold(name, mode_color['local'])
                     print_bold('.\n')
                 else:
-                    print_bold('main', main_color)
+                    print_bold('main', mode_color['main'])
                     print_bold(' TODO list already exists in ')
-                    print_bold(todolist['path'], main_color)
+                    print_bold(data['path'], mode_color['main'])
                     print_bold('.\n')
 
-        elif action == 'settings':
-            if todolist['error'] == 1:
+        elif action == 'mode':
+            if data['error'] == 1:
                 print_bold('Wrong index.\n')
 
-            elif todolist['error'] == 2:
-                print_bold('Mode already set to ')                    
-                print_bold(todolist['mode'], 'red')
+            elif data['error'] == 2:
+                print_bold('Mode already set to ')
+                print_bold(data['mode'], mode_color[data['mode']])
                 print_bold('.\n')
-                    
-        # exit
-        exit(todolist['error'])
 
-    
+        # exit
+        exit(data['error'])
+
+
 if __name__ == '__main__':
     main()
